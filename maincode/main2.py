8411e8df87f439cac1fae47831c9fcf50c51990c
Geby Jaff <84095175+spicylemonade@users.noreply.github.com>
2023-03-15 20:03:56 -0400

    Update main2.py

--- maincode/main2.py
+++ maincode/main2.py
@@ -157,8 +157,8 @@ def balance(IC,time,m,dt,l_robot,w_robot,l_charge,w_charge,h_charge,KDx,KPt,KDt)
 
 class MyRobot(wpilib.TimedRobot):
     """Main robot class."""
-    preSet_middlePeg = 1 #confirm later
-    preSet_topPeg = 1.5 # confirm later
+    preSet_middlePeg = 25.97 #confirm later
+    preSet_topPeg = 28.5 # confirm later
 
     isExtended = False
 
@@ -166,6 +166,9 @@ class MyRobot(wpilib.TimedRobot):
 
 
         wpilib.CameraServer.launch('vision.py:main')
+        self.arm1 = False
+        self.arm2 = False
+        self.arm3 = False
         self.joystick = Joystick(0)
         self.controller = interfaces.GenericHID(0)
         self.top_left = CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless)
@@ -250,7 +253,7 @@ class MyRobot(wpilib.TimedRobot):
         self.blm.setPosition(0.0)
         self.trm.setPosition(0.0)
         self.brm.setPosition(0.0)
-        self.arm_encoder.setPosition(0)
+        #self.arm_encoder.setPosition(0)
 
         """Called only at the beginning of teleoperated mode."""
         # self.loops = 0
@@ -266,8 +269,9 @@ class MyRobot(wpilib.TimedRobot):
         self.top_left.set(power*(((x*self.rev*(1/self.wheel_circ))-self.tlm.getPosition())/(x*self.rev*(1/self.wheel_circ))))
         self.bottom_right.set(power*(((x*self.rev*(1/self.wheel_circ))-self.brm.getPosition())/(x*self.rev*(1/self.wheel_circ))))
         self.top_right.set(power*(((x*self.rev*(1/self.wheel_circ))-self.trm.getPosition())/(x*self.rev*(1/self.wheel_circ))))
-    def arm_orientation(self,power,power_2,x,dx,r):
-        self.arm.set(power*((r-x)+power_2*(dx))/r)
+    def arm_orientation(self,power,x,r):
+            self.arm.set(power*((r-x)/r))
+            #self.arm.set(power*((r-x)+power_2*(dx))/r)
 
     def teleopPeriodic(self):
         """Called every 20ms in teleoperated mode"""
@@ -295,17 +299,45 @@ class MyRobot(wpilib.TimedRobot):
         self.mec_drive.driveCartesian(joy_y * -0.5, joy_x * 0.5, joy_z * 0.5) #cant be on at same time as controll_rev()
 
 
+        #self.logger.info(self.arm_encoder.getPosition())
+        #self.logger.info(self.tlm.getPosition())
         self.logger.info(self.arm_encoder.getPosition())
+        #middle node(cone) 25.97
+        #top node (28.5)
 
+
+        if self.arm1:
+            self.arm_orientation(0.5,self.arm_encoder.getPosition(),self.preSet_topPeg)
+        elif self.arm2:
+            self.arm_orientation(0.5,self.arm_encoder.getPosition(),self.preSet_middlePeg)
+        elif self.arm3 and self.arm_encoder.getPosition()>=15:
+            # self.arm_orientation(0.005,self.arm_encoder.getPosition(),12)
+            self.arm.set(0)
+        elif self.arm3 and (self.arm_encoder.getPosition()<=15 and self.arm_encoder.getPosition()>=10) :
+            # self.arm_orientation(0.005,self.arm_encoder.getPosition(),12)
+            self.arm.set(0.0005)
+        elif self.arm3 and (self.arm_encoder.getPosition()<=10 and self.arm_encoder.getPosition()>=4):
+            # self.arm_orientation(0.005,self.arm_encoder.getPosition(),12)
+            self.arm.set(0.005)
+        else:
+            self.arm.set(0)
 
         # Move a motor with a Joystick
         #self.controll_rev(0.3,2*math.pi*4)
-        # if self.controller.getRawButton(3):
-        #     self.arm_orientation(0.5,0.1,self.arm_encoder.getPosition(),self.arm_encoder.getVelocity(),self.preSet_topPeg)
-        # if self.controller.getRawButton(4):
-        #     self.arm_orientation(0.5,0.1,self.arm_encoder.getPosition(),self.arm_encoder.getVelocity(),self.preSet_middlePeg)
-        # if self.controller.getRawButton(5):
-        #     self.arm_orientation(0.5,0.1,self.arm_encoder.getPosition(),self.arm_encoder.getVelocity(),0)
+        if self.controller.getRawButton(9):
+            self.arm1 = True
+            self.arm2 = False
+            self.arm3 = False
+        if self.controller.getRawButton(10):
+            self.arm2 = True
+            self.arm1 = False
+            self.arm3 = False
+            self.logger.info("im pressed")
+        if self.controller.getRawButton(12):
+            self.arm3 = True
+            #self.arm.set(0)
+            self.arm1 = False
+            self.arm2 = False
 
 
         #self.tlm.setPosition(0)
