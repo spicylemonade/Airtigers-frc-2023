737d56c0b0c97ec697059ee61bcc1c68b7d58b7f
Geby Jaff <84095175+spicylemonade@users.noreply.github.com>
2023-03-21 19:09:13 -0400

    Update robot.py

--- maincode/robot.py
+++ maincode/robot.py
@@ -2,22 +2,81 @@
 import math
 
 import wpilib
-from wpilib import PneumaticsModuleType, Compressor, Joystick, DoubleSolenoid, drive,interfaces, DigitalInput
+from wpilib import PneumaticsModuleType, Compressor, Joystick, DoubleSolenoid, drive, interfaces, DigitalInput,PWMTalonSRX
 from rev import CANSparkMax, RelativeEncoder, CANSparkMaxLowLevel
+from navx import AHRS
+
+import numpy as np
+from wpimath import controller
+
+
 class MyRobot(wpilib.TimedRobot):
     """Main robot class."""
+    preSet_middlePeg = 25.97  # confirm later
+    preSet_topPeg = 32.5  # confirm later
 
+    isExtended = False
+    scored, mobility, mobility2,balanced = False, False, False,False
+    scored = True
+    deg_to_count = 90*(42/360)
+    arm1 = False
+    arm2 = False
+    arm3 = False
+    wheel_circ = 2 * math.pi * 4
+    rev = 6.78
 
-    def robotInit(self):
+    def __init__(self):
+        wpilib.TimedRobot.__init__(self)
         self.joystick = Joystick(0)
         self.controller = interfaces.GenericHID(0)
+        #self.navx = AHRS(wpilib.SerialPort.Port.kUSB)
+
+        self.xbox = interfaces.GenericHID(1)
         self.top_left = CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless)
         self.bottom_left = CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless)
         self.top_right = CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless)
         self.bottom_right = CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless)
-        self.arm = CANSparkMax(5,CANSparkMaxLowLevel.MotorType.kBrushless)
+        self.arm = CANSparkMax(5, CANSparkMaxLowLevel.MotorType.kBrushless)
 
-        self.armlimit = DigitalInput(0)
+        self.claw_lead =  CANSparkMax(17, CANSparkMaxLowLevel.MotorType.kBrushless)
+        self.claw1 = PWMTalonSRX(7)
+        self.claw2=  PWMTalonSRX(8)
+
+
+        self.armlimit_bottom = DigitalInput(1)
+        self.armlimit_top = DigitalInput(0)
+
+        self.mec_drive = drive.MecanumDrive(self.top_left, self.bottom_left, self.top_right, self.bottom_right)
+
+
+
+        self.tlm = self.top_left.getEncoder()
+        self.blm = self.bottom_left.getEncoder()
+        self.trm = self.top_right.getEncoder()
+        self.brm = self.bottom_right.getEncoder()
+        self.arm_encoder = self.arm.getEncoder()
+
+        self.comp = Compressor(0, PneumaticsModuleType.CTREPCM)
+
+        #self.comp.enableDigital()
+
+        self.doubles = DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1)
+
+        self.doubles.set(DoubleSolenoid.Value.kReverse)
+        self.arm_encoder.setPosition(0)
+        # self.kP = 0.5
+        # self.kI = 0.1
+        # self.kD = 0.0
+        # self.pid = controller.PIDController(self.kP, self.kI, self.kD)
+        #self.accel = ADXL345_I2C(I2C.Port.kOnboard,20,0x53)
+        #self.pitch = math.acos(9.81/self.accel.getY())
+
+
+    def robotInit(self):
+
+        wpilib.CameraServer.launch('vision.py:main')
+
+
 
 
         self.top_right.setInverted(True)
@@ -25,38 +84,89 @@ class MyRobot(wpilib.TimedRobot):
         self.top_left.setInverted(False)
         self.bottom_right.setInverted(True)
 
-        self.mec_drive = drive.MecanumDrive(self.top_left, self.bottom_left, self.top_right, self.bottom_right)
 
-
-        self.tlm = self.top_left.getEncoder()
-        self.blm = self.bottom_left.getEncoder()
-        self.trm = self.top_right.getEncoder()
-        self.brm = self.bottom_right.getEncoder()
 
         self.tlm.setPosition(0.0)
         self.blm.setPosition(0.0)
         self.trm.setPosition(0.0)
         self.brm.setPosition(0.0)
         """Robot-wide initialization code should go here."""
-        self.comp = Compressor(0, PneumaticsModuleType.CTREPCM)
-        #self.comp.enableDigital()
-        self.wheel_circ = 2*math.pi*4
 
-        self.doubles = DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1)
-        self.rev = 6.78
+
+
+
 
 
     def autonomousInit(self):
+        self.tlm.setPosition(0.0)
+        self.blm.setPosition(0.0)
+        self.trm.setPosition(0.0)
+        self.brm.setPosition(0.0)
+        self.arm_encoder.setPosition(0.0)
         """Called only at the beginning of autonomous mode."""
+        # self.pid.setOutputRange(-0.5, 0.5)
+        self.pid.setSetpoint(0)
+        self.pid.enableContinuousInput(-180,180)
+        self.navx.reset()
         pass
 
     def autonomousPeriodic(self):
         """Called every 20ms in autonomous mode."""
-        # U = Inputs(0, 0, 0, 0, 0, 0)
-        # u[0] and u[1] for drive
-        # u[2] for arm
-        # u[3] piston
-        pass
+
+        self.logger.info(self.navx.getPitch())
+        # if self.scored == False:
+        #     # move forward 38 inches
+        #     self.scoring(9)
+        # elif (self.tlm.getPosition() > -60) and (self.mobility == False):
+        #
+        #     self.mec_drive.driveCartesian(-0.3, 0, 0)
+        #
+        # elif (math.fabs(self.navx.getPitch())<20 ) and self.balanced == False:
+        #     # iffy rate
+        #     self.mobility = True
+        #     self.mec_drive.driveCartesian(0.3, 0, 0)
+        #
+        # else:
+        #     self.balanced = True
+        #     self.mec_drive.driveCartesian(0.3*((0-self.navx.getPitch())/180), 0, 0)
+        if self.scored == False:
+            # move forward 38 inches
+            self.scoring()
+        elif (self.tlm.getPosition() > -50.8) and (self.mobility == False):
+
+            self.mec_drive.driveCartesian(-0.5*((-50.8-self.tlm.getPosition())/-50.8), 0, 0)
+
+        elif (self.mobility2 == False) and not(-38 > self.tlm.getPosition() > -45):
+            self.mobility = True
+
+            # self.mec_drive.driveCartesian(0, 0.3, 0) #y0.3
+            self.mec_drive.driveCartesian(0, 0.5*((-40-self.tlm.getPosition())/-40), 0)
+        elif (-38 > self.tlm.getPosition() > -45) and self.mobility2 == False:
+            self.mobility2 = True
+
+        elif (math.fabs(self.navx.getPitch())<=2 ) and self.balanced == False:
+            # iffy rate
+            self.mobility2 = True
+            self.mec_drive.driveCartesian(0.3, 0, 0) #0.3
+
+        else:
+            self.balanced = True
+            # self.mec_drive.driveCartesian(0.15*((self.navx.getPitch())),0,0)
+            # self.pid.setSetpoint(0)
+            # self.pid.setPID(self.kP, self.kI, 0.1)
+            #self.pid.enableContinuousInput(-15,15)
+
+            # Check for motor stall
+            # if self.pi:
+            #     self.mec_drive.driveCartesian(0,0,0)
+            # self.mec_drive.driveCartesian(self.pid.calculate(self.navx.getPitch(),0)*0.15,0,0)
+            # if self.navx.getPitch()==0:
+            #     self.mec_drive.driveCartesian(0,0,0)
+
+            # self.logger.info(self.pid.calculate(self.navx.getPitch(),0))
+            pass
+
+    pass
 
     def disabledInit(self):
         """Called only at the beginning of disabled mode."""
@@ -67,72 +177,153 @@ class MyRobot(wpilib.TimedRobot):
         pass
 
     def teleopInit(self):
+        self.doubles.set(DoubleSolenoid.Value.kReverse)
         self.tlm.setPosition(0.0)
         self.blm.setPosition(0.0)
         self.trm.setPosition(0.0)
         self.brm.setPosition(0.0)
+        # self.arm_encoder.setPosition(0)
 
         """Called only at the beginning of teleoperated mode."""
         # self.loops = 0
         # self.timer.reset()
         # self.timer.start()
-    # def controll_rev(self,power,x):
-    #     self.bottom_left.set(power*(((x*self.rev)-self.blm.getPosition())/(x*self.rev)))
-    #     self.top_left.set(power*(((x*self.rev)-self.tlm.getPosition())/(x*self.rev)))
-    #     self.bottom_right.set(power*(((x*self.rev)-self.brm.getPosition())/(x*self.rev)))
-    #     self.top_right.set(power*(((x*self.rev)-self.trm.getPosition())/(x*self.rev)))
-    def controll_rev(self,power,x):
-        self.bottom_left.set(power*(((x*self.rev*(1/self.wheel_circ))-self.blm.getPosition())/(x*self.rev*(1/self.wheel_circ))))
-        self.top_left.set(power*(((x*self.rev*(1/self.wheel_circ))-self.tlm.getPosition())/(x*self.rev*(1/self.wheel_circ))))
-        self.bottom_right.set(power*(((x*self.rev*(1/self.wheel_circ))-self.brm.getPosition())/(x*self.rev*(1/self.wheel_circ))))
-        self.top_right.set(power*(((x*self.rev*(1/self.wheel_circ))-self.trm.getPosition())/(x*self.rev*(1/self.wheel_circ))))
     def teleopPeriodic(self):
         """Called every 20ms in teleoperated mode"""
+        #self.logger.info(self.pitch)
 
         if self.controller.getRawButton(5):
+            self.isExtended = True
             self.doubles.set(DoubleSolenoid.Value.kForward)
         elif self.controller.getRawButton(6):
+            self.isExtended = False
             self.doubles.set(DoubleSolenoid.Value.kReverse)
         else:
             self.doubles.set(DoubleSolenoid.Value.kOff)
 
         # self.driver()
-        joy_y = self.joystick.getY()
-        joy_x = self.joystick.getX()
-        joy_z = self.joystick.getZ()
 
-        if math.fabs(self.joystick.getY())<0.3:
-            joy_y = 0
-        if math.fabs(self.joystick.getX())<0.3:
-            joy_x = 0
-        if math.fabs(self.joystick.getZ())<0.3:
-            joy_z = 0
-        self.mec_drive.driveCartesian(joy_y * -0.5, joy_x * 0.5, joy_z * 0.5)
+        self.joy_y = self.joystick.getY()
+        self.joy_x = self.joystick.getX()
+        self.joy_z = self.joystick.getZ()
+        #self.logger.info("ACCEL: " + str(self.accel.getAcceleration(ADXL345_I2C.Axes.kAxis_Z)))
+
+        if math.fabs(self.joystick.getY()) < 0.35:
+            self.joy_y = 0
+        if math.fabs(self.joystick.getX()) < 0.35:
+            self.joy_x = 0
+        if math.fabs(self.joystick.getZ()) < 0.35:
+            self.joy_z = 0
+        self.mec_drive.driveCartesian(self.joy_y * -0.7, self.joy_x * 0.7,self.joy_z * 0.7)  # cant be on at same time as controll_rev()
+        #self.balancing()
+        #self.clawer()
+
+        #arm
+        if not self.controller.getRawButton(1):
+            self.arm_controller()
+        else:
+            if not self.arm_encoder.getPosition() >= self.preSet_middlePeg:
+                self.arm.set(0.5)
+                self.arm3 = True
+                self.arm1 = False
+                self.arm2 = False
 
 
-        if self.controller.getRawButton(3):
-            self.arm.set(0.5)
-        elif self.controller.getRawButton(4):
-            self.arm.set(-0.5)
-
+        #claw
+        #self.clawer()
 
         # Move a motor with a Joystick
-        #self.controll_rev(0.3,2*math.pi*4)
+        # self.controll_rev(0.3,2*math.pi*4)
 
-        self.logger.info(self.tlm.getPosition())
-        if(self.armlimit.get()):
+        self.logger.info(self.navx.getPitch())
+        if self.controller.getRawButton(8):
+            self.arm1 = True
+            self.arm2 = False
+            self.arm3 = False
+        if self.controller.getRawButton(10):
+            self.arm2 = True
+            self.arm1 = False
+            self.arm3 = False
+        if self.controller.getRawButton(12):
+            self.arm3 = True
+            self.arm1 = False
+            self.arm2 = False
+
+
+
+    def scoring(self):
+
+        #self.control_rev(1,10)
+        self.arm1 = True
+        self.arm2 = False
+        self.arm3 = False
+        self.isExtended = True
+        self.doubles.set(DoubleSolenoid.Value.kForward)
+        #self.arm_controller()
+        # work claw when armp is at top peg position
+        # scored = True when claw released
+        if self.arm_encoder.getPosition() >=self.preSet_middlePeg:
+            self.claw_lead.set(0.5 * ((6 - self.claw_lead.getEncoder().getPosition()) / 6))
+        if self.claw_lead.getEncoder().getPosition() >= self.deg_to_count:
+            self.claw1.set(0.5 * ((self.deg_to_count - self.claw1.getEncoder().getPosition()) / self.deg_to_count))
+        if self.claw1.getEncoder().getPosition() >= 6:
+            self.scored = True
+            self.arm1 = False
+            self.arm2 = False
+            self.arm3 = True
+            self.isExtended = False
+            self.doubles.set(DoubleSolenoid.Value.kReverse)
+
+
+
+
+
+    def control_rev(self,power,x):
+        self.mec_drive.driveCartesian(0.15*power,0,0)
+
+    # def control_rev(self,power,x):
+    #     self.bottom_left.set(power*(((x*self.rev*(1/self.wheel_circ))-self.blm.getPosition())/(x*self.rev*(1/self.wheel_circ))))
+    #     self.top_left.set(power*(((x*self.rev*(1/self.wheel_circ))-self.tlm.getPosition())/(x*self.rev*(1/self.wheel_circ))))
+    #     self.bottom_right.set(power*(((x*self.rev*(1/self.wheel_circ))-self.brm.getPosition())/(x*self.rev*(1/self.wheel_circ))))
+    #     self.top_right.set(power*(((x*self.rev*(1/self.wheel_circ))-self.trm.getPosition())/(x*self.rev*(1/self.wheel_circ))))
+
+    # self.mec_drive.driveCartesian(-power*(((x*self.rev*(1/self.wheel_circ))-self.trm.getPosition())/(
+    # x*self.rev*(1/self.wheel_circ))), joy_x * 0.5, joy_z * 0.5)
+
+    def arm_orientation(self, power, x, r):
+        self.arm.set(power * ((r - x) / r))
+
+    @staticmethod
+    def limit(n, minn, maxn):
+        return max(min(maxn, n), minn)
+
+    def arm_controller(self):
+        if self.arm1:
+            self.arm_orientation(0.7, self.arm_encoder.getPosition(), self.preSet_topPeg)
+        elif self.arm2:
+            self.arm_orientation(0.7, self.arm_encoder.getPosition(), self.preSet_middlePeg)
+        elif self.arm3:
+            self.arm.set(0.000001)
+        else:
             self.arm.set(0)
+        # self.arm.set(power*((r-x)+power_2*(dx))/r)
+    def clawer(self):
 
-        #self.tlm.setPosition(0)
-        #6.78
-        # self.motor.set(self.lstick.getY())
-        #
-        # # Print out the number of loop iterations passed every second
-        # self.loops += 1
+        if self.xbox.getRawButton(4):
+            self.claw_lead.set(-0.15)
+        elif self.xbox.getRawButton(5):
+            self.claw_lead.set(0.15)
+        else:
+            self.claw_lead.set(0)
+        left_axis=self.xbox.getRawAxis(2)
+        #self.claw_lead.set(0.5 * ((6 - self.claw_lead.getEncoder().getPosition()) / 6))
+        #left_axis = 0 if left_axis<= 0.1 else left_axis
+        #right_axis=self.xbox.getRawAxis(3)
+        #right_axis = 0 if right_axis<= 0.1 else right_axis
+        #self.claw1.set(left_axis)
 
-        # if self.timer.advanceIfElapsed(1):
-        #     self.logger.info("%d loops / second", self.loops)
-        #     self.loops = 0
+
+
 
 
 if __name__ == "__main__":
